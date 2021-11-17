2bfcdb190d5d51faf4c04e541bf8ebec651dc83c
EvgeniyaMelenteva <9210793@gmail.com>
2021-11-17 23:15:05 +0300

    4 commit

--- app/src/main/java/com/example/zodiacapp/HomeFragment.kt
+++ app/src/main/java/com/example/zodiacapp/HomeFragment.kt
@@ -1,10 +1,12 @@
 package com.example.zodiacapp
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.fragment.app.Fragment
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -33,27 +35,19 @@ class HomeFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+
+        val view = inflater.inflate(R.layout.fragment_home, container, false)
+        val buttonOpenURL: Button = view.findViewById(R.id.buttonOpenURL)
+
+        val intent = Intent(Intent.ACTION_MAIN)
+        intent.addCategory(Intent.CATEGORY_APP_EMAIL)
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+
+        buttonOpenURL.setOnClickListener {
+            startActivity(intent)
+        }
+        return view
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment HomeFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            HomeFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
+
 }
--- app/src/main/java/com/example/zodiacapp/MainActivity.kt
+++ app/src/main/java/com/example/zodiacapp/MainActivity.kt
@@ -2,7 +2,6 @@ package com.example.zodiacapp
 
 import android.content.Intent
 import android.os.Bundle
-import android.os.PersistableBundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
@@ -16,7 +15,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.fragment_home)
+        setContentView(R.layout.activity_main)
 
 
 
@@ -29,6 +28,7 @@ class MainActivity : AppCompatActivity() {
                     supportFragmentManager
                         .beginTransaction()
                         .replace(R.id.fragment_container, homeFragment)
+                        .commit()
                 }
                 R.id.zodiacSigns -> {
                     val zodiacFragment = ZodiacFragment()
@@ -51,21 +51,12 @@ class MainActivity : AppCompatActivity() {
             true
         }
 
-        val buttonOpenURL: Button = findViewById(R.id.buttonOpenURL)
-
-        val intent = Intent(Intent.ACTION_MAIN)
-        intent.addCategory(Intent.CATEGORY_APP_EMAIL)
-        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-
-        buttonOpenURL.setOnClickListener {
-            startActivity(intent)
 
       bottomMenu.selectedItemId = savedInstanceState?.getInt(LAST_SELECTED_ITEM) ?: R.id.home
         }
 
-        }
         override fun onSaveInstanceState(outState: Bundle) {
         outState.putInt(LAST_SELECTED_ITEM,bottomMenu.selectedItemId)
         super.onSaveInstanceState(outState)
         }
-    }
+}
